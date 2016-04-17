5b7c7bad9836702148a780dfe294d13c8b97bd4f
Alex Whitney <alex.whitney@cantab.net>
2016-04-17 11:31:12 -0600

    mock collision detection

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #[macro_use]
 extern crate glium;
 extern crate nalgebra as na;
+extern crate rand;
 
 use na::{Vec1, Vec2, Norm, Absolute, Rotate};
 use glium::{DisplayBuild, Surface, VertexBuffer};
@@ -15,103 +16,12 @@ use std::collections::{HashMap, HashSet};
 const ACCEL: f32 = 0.01;
 const STEP: f32 = 0.02;
 
-#[derive(Clone, Debug)]
-struct Board {
-    ship: Ship,
-    mooks: HashMap<u32, Mook>,
-    bullets: HashMap<u32, Bullet>,
-    bulletct: u32
-}
-
-impl Board {
-    fn new() -> Board {
-        Board {
-            ship: Ship::new(),
-            mooks: HashMap::new(),
-            bullets: HashMap::new(),
-            bulletct: 0
-        }
-    }
-
-    fn step(&mut self) {
-        self.ship.step();
-        for b in self.bullets.values_mut() {
-            b.step()
-        };
-        for m in self.mooks.values_mut() {
-            m.step()
-        };
-    }
-
-    fn fire(&mut self) {
-        let bullet = self.ship.fire();
-        self.bullets.insert(self.bulletct, bullet);
-        self.bulletct += 1;
-    }
-
-    fn tidy(&mut self) {
-        let mut to_rm = Vec::new();
-        for (&id, b) in &self.bullets {
-            if b.pos.sqnorm() > 2.0 {
-                to_rm.push(id)
-            }
-        }
-        for id in to_rm {
-            self.bullets.remove(&id).unwrap();
-        }
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-struct Mook {
-    pos: Vec2<f32>,
-    vel: Vec2<f32>,
-    health: f32
-}
-
-impl Mook {
-    fn step(&mut self) {
-    }
-}
-
 #[derive(Copy, Clone, Debug)]
 struct Ship {
     pos: Vec2<f32>,
     vel: Vec2<f32>,
     orient: f32,
     health: f32
-}
-
-#[derive(Copy, Clone, Debug)]
-struct Bullet {
-    pos: Vec2<f32>,
-    vel: Vec2<f32>,
-}
-
-impl Bullet {
-    fn new(pos: Vec2<f32>, vel: Vec2<f32>) -> Bullet {
-        Bullet {
-            pos: pos,
-            vel: vel
-        }
-    }
-
-    fn vertices() -> Vec<Vertex> {
-        vec![
-            Vertex::new(-0.1, -1.0),
-            Vertex::new(-0.1,  1.0),
-            Vertex::new( 0.1,  1.0),
-            Vertex::new( 0.1, -1.0),
-        ]
-    }
-
-    fn step(&mut self) {
-        self.pos = self.pos + self.vel * STEP
-    }
-
-    fn orient(&self) -> f32 {
-        na::angle_between(&Vec2::x(), &self.vel)
-    }
 }
 
 impl Ship {
@@ -173,7 +83,7 @@ impl Ship {
     }
 
     fn fire(&self) -> Bullet {
-        Bullet::new(self.pos, self.orient_vec() * 1.1)
+        Bullet::new(self.pos, self.orient_vec() * 2.0)
     }
 
     fn orient_vec(&self) -> Vec2<f32> {
@@ -181,9 +91,158 @@ impl Ship {
     }
 }
 
+impl Default for Ship {
+    fn default() -> Ship {
+        Ship::new()
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Bullet {
+    pos: Vec2<f32>,
+    vel: Vec2<f32>,
+}
+
+impl Bullet {
+    fn new(pos: Vec2<f32>, vel: Vec2<f32>) -> Bullet {
+        Bullet {
+            pos: pos,
+            vel: vel
+        }
+    }
+
+    fn vertices() -> Vec<Vertex> {
+        vec![
+            Vertex::new(-0.1, -1.0),
+            Vertex::new(-0.1,  1.0),
+            Vertex::new( 0.1,  1.0),
+            Vertex::new( 0.1, -1.0),
+        ]
+    }
+
+    fn step(&mut self) {
+        self.pos = self.pos + self.vel * STEP
+    }
+
+    fn orient(&self) -> f32 {
+        na::angle_between(&Vec2::x(), &self.vel)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Mook {
+    pos: Vec2<f32>,
+    vel: Vec2<f32>,
+    orient: f32,
+    health: f32,
+    level: u32
+}
+
+impl Mook {
+    fn new(pos: Vec2<f32>, vel: Vec2<f32>, orient: f32) -> Mook {
+        Mook {
+            pos: pos,
+            vel: vel,
+            orient: orient,
+            health: 1.0,
+            level: 5
+        }
+    }
+
+    fn random() -> Mook {
+        Mook::new(random_vec(), random_vec() * 0.5, rand::random::<f32>() * 2.0 * PI)
+    }
+
+    fn step(&mut self) {
+        self.pos = self.pos + self.vel * STEP;
+        self.orient += 0.1;
+        wrap(&mut self.pos);
+    }
+
+    fn vertices() -> Vec<Vertex> {
+        vec![
+            Vertex::new(-0.5, -0.5),
+            Vertex::new(-0.5,  0.5),
+            Vertex::new( 0.5,  0.5),
+            Vertex::new( 0.5, -0.5),
+        ]
+    }
+
+}
+
+#[derive(Clone, Debug, Default)]
+struct Board {
+    ship: Ship,
+    mooks: HashMap<u32, Mook>,
+    bullets: HashMap<u32, Bullet>,
+    bulletct: u32,
+    mookct: u32,
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            ship: Ship::new(),
+            mooks: HashMap::new(),
+            bullets: HashMap::new(),
+            ..Default::default()
+        }
+    }
+
+    fn step(&mut self) {
+        self.ship.step();
+        for b in self.bullets.values_mut() {
+            b.step()
+        };
+        for m in self.mooks.values_mut() {
+            m.step()
+        };
+        self.sweep();
+    }
+
+    fn collision_detect(&mut self) {
+        for m in self.mooks {
+            for b in self.bullets {
+                if near_detect(m, b) && exact_detect(m, b) {
+                    if let Some((m1, m2)) = mook.explode() {
+                    }
+                }
+            }
+        }
+    }
+
+    fn fire(&mut self) {
+        let bullet = self.ship.fire();
+        self.bullets.insert(self.bulletct, bullet);
+        self.bulletct += 1;
+    }
+
+    fn sweep(&mut self) {
+        let mut to_rm = Vec::new();
+        for (&id, b) in &self.bullets {
+            if b.pos.sqnorm() > 2.0 {
+                to_rm.push(id)
+            }
+        }
+        for id in to_rm {
+            self.bullets.remove(&id).unwrap();
+        }
+    }
+
+    fn add_mook(&mut self) {
+        self.mooks.insert(self.mookct, Mook::random());
+        self.mookct += 1;
+    }
+}
+
 fn wrap(v: &mut Vec2<f32>) {
     if v.x.abs() > 1.05 { v.x *= -1.0 }
     if v.y.abs() > 1.05 { v.y *= -1.0 }
+}
+
+fn random_vec() -> Vec2<f32> {
+    Vec2::new(rand::random::<f32>() * 2.0 - 1.0,
+              rand::random::<f32>() * 2.0 - 1.0)
 }
 
 const VERTEX_SHADER : &'static str = r#"
@@ -293,14 +352,25 @@ impl Window {
     fn render(&self, board: &Board, assets: &Assets) {
         let mut target = self.display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
+        self.render_ship(&board.ship, &mut target, assets);
+        for b in board.bullets.values() {
+            self.render_bullet(b, &mut target, assets)
+        }
+        for m in board.mooks.values() {
+            self.render_mook(m, &mut target, assets)
+        }
+        target.finish().unwrap();
+    }
+
+    fn render_ship(&self, ship: &Ship, target: &mut glium::Frame, assets: &Assets) {
         target.draw(&assets.ship_vx_buf,
                     &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
                     &self.program,
                     &uniform!(
                         scale: scale_mat(0.1),
-                        rot: rot_mat(board.ship.orient),
+                        rot: rot_mat(ship.orient),
                         rot_offset: rot_mat(PI / -2.0),
-                        trans: trans_mat(board.ship.pos.x, board.ship.pos.y)
+                        trans: trans_mat(ship.pos.x, ship.pos.y)
                     ),
                     &assets.draw_params).unwrap();
         if self.events.0.contains(&VirtualKeyCode::Up) {
@@ -309,25 +379,37 @@ impl Window {
                         &self.program,
                         &uniform!(
                             scale: scale_mat(0.1),
-                            rot: rot_mat(board.ship.orient),
+                            rot: rot_mat(ship.orient),
                             rot_offset: rot_mat(PI / -2.0),
-                            trans: trans_mat(board.ship.pos.x, board.ship.pos.y)
+                            trans: trans_mat(ship.pos.x, ship.pos.y)
                         ),
                         &assets.draw_params).unwrap();
         }
-        for b in board.bullets.values() {
-            target.draw(&assets.bullet_vx_buf,
-                        &glium::index::NoIndices(glium::index::PrimitiveType::LineLoop),
-                        &self.program,
-                        &uniform!(
-                            scale: scale_mat(0.01),
-                            rot: rot_mat(b.orient()),
-                            rot_offset: rot_mat(PI / -2.0),
-                            trans: trans_mat(b.pos.x, b.pos.y)
-                        ),
-                        &assets.draw_params).unwrap();
-        }
-        target.finish().unwrap();
+    }
+
+    fn render_bullet(&self, bullet: &Bullet, target: &mut glium::Frame, assets: &Assets) {
+        target.draw(&assets.bullet_vx_buf,
+                    &glium::index::NoIndices(glium::index::PrimitiveType::LineLoop),
+                    &self.program,
+                    &uniform!(
+                        scale: scale_mat(0.01),
+                        rot: rot_mat(bullet.orient()),
+                        rot_offset: rot_mat(PI / -2.0),
+                        trans: trans_mat(bullet.pos.x, bullet.pos.y)
+                    ),
+                    &assets.draw_params).unwrap();
+    }
+    fn render_mook(&self, mook: &Mook, target: &mut glium::Frame, assets: &Assets) {
+        target.draw(&assets.mook_vx_buf,
+                    &glium::index::NoIndices(glium::index::PrimitiveType::LineLoop),
+                    &self.program,
+                    &uniform!(
+                        scale: scale_mat(0.05 * mook.level as f32),
+                        rot: rot_mat(mook.orient),
+                        rot_offset: rot_mat(0.0),
+                        trans: trans_mat(mook.pos.x, mook.pos.y)
+                    ),
+                    &assets.draw_params).unwrap();
     }
 }
 
@@ -335,6 +417,7 @@ struct Assets {
     ship_vx_buf: VertexBuffer<Vertex>,
     thruster_vx_buf: VertexBuffer<Vertex>,
     bullet_vx_buf: VertexBuffer<Vertex>,
+    mook_vx_buf: VertexBuffer<Vertex>,
     draw_params: glium::DrawParameters<'static>
 }
 
@@ -349,6 +432,7 @@ impl Assets {
             ship_vx_buf: VertexBuffer::new(&window.display, &Ship::vertices()).unwrap(),
             thruster_vx_buf: VertexBuffer::new(&window.display, &Ship::thrust_vertices()).unwrap(),
             bullet_vx_buf: VertexBuffer::new(&window.display, &Bullet::vertices()).unwrap(),
+            mook_vx_buf: VertexBuffer::new(&window.display, &Mook::vertices()).unwrap(),
             draw_params: draw_params
         }
     }
@@ -374,15 +458,16 @@ fn game_loop(mut board: Board, mut window: Window) {
                 Up => board.ship.accel(),
                 Down => board.ship.decel(),
                 Space => board.fire(),
-                Q => return
+                Q => return,
+                _ => ()
             }
         }
-        board.tidy();
     }
 }
 
 fn main() {
-    let board = Board::new();
+    let mut board = Board::new();
+    board.add_mook();
     let window = Window::new();
     game_loop(board, window);
 }
